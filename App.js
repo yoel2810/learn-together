230bf5cb37d3689ffae78ae42c78f0b7483efb05
Yoel Hartman <yoel2810@gmail.com>
2023-05-10 22:28:15 +0300

    Added login page

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomePage from './src/screens/HomePage/HomePage';
+import MessageBoard from './src/screens/MessagesBoard/MessageBoard';
+import HookExample from './src/components/HookExample/HookExample';
+import LoginScreen from './src/screens/LoginScreen/LoginScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoginScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
